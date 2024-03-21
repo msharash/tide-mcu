e0e7f53d2afa7b56f384ca81456d3af0144f42a7
msharash <91023831+msharash@users.noreply.github.com>
2024-03-20 22:55:32 -0400

    Update mux.ino

--- mux.ino
+++ mux.ino
@@ -1,8 +1,19 @@
 #include <CAN.h>
 #include <Arduino.h>
 
-volatile int mux;
-volatile int i = 0;
+volatile float mux;
+volatile float BP1_P, BP2_P, BP3_P, BP4_P, BP5_P, BP6_P;
+volatile int16_t BP1_T, BP2_T, BP3_T, BP4_T, BP5_T, BP6_T;
+volatile float BP_P_Supply;
+volatile int16_t BP_Flow;
+volatile float Exch_P, Suct_P, Disch_P;
+volatile int16_t Exch_T, Master_T;
+volatile float IN1_P, IN2_P, IN_P_Supply;
+volatile int16_t IN_Temp;
+volatile int16_t IN_Flow;
+volatile float M_P, M_P_Supply;
+volatile int16_t M_T, M_Flow;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,6 +33,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  static int i;
+  
   digitalWrite(A11, i & 1);
   digitalWrite(A12, i & (1<<1));
   digitalWrite(A13, i & (1<<2));
@@ -39,4 +52,6 @@ void loop() {
   CAN.beginExtendedPacket(400000000);
   CAN.write(mux);
   CAN.endPacket();
+  
+  delay(100);
 }
